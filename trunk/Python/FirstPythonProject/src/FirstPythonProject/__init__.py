458fcf8c28c65b123d959408ca8380768ffa409f
larryzzl <larryzzl@dd12737e-0f7a-11de-aa17-9100d24c02a7>
2010-03-30 01:19:19 +0000

    del for re-org

--- trunk/Python/FirstPythonProject/src/FirstPythonProject/__init__.py
+++
@@ -1,9 +0,0 @@
-def buildConnectionString(params):
-    """test function
-    
-    Return string"""
-    return ";".join(["%s=%s" % (k, v) for k, v in params.items()])
-
-if __name__ == "__main__":
-    myParams = {"server":"abc", "uid":"sa"}
-    print buildConnectionString(myParams)
