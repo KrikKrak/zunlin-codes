57c6fb342ac350f3a8c93282ba158f320f975abb
larryzzl <larryzzl@dd12737e-0f7a-11de-aa17-9100d24c02a7>
2010-04-25 08:48:40 +0000

    hook up server-client, database based workflow

--- Python/Project/Python Project/GoogleAppEngine/family-recipe/FamilyRecipe/main.py
+++ Python/Project/Python Project/GoogleAppEngine/family-recipe/FamilyRecipe/main.py
@@ -1,6 +1,8 @@
 import os
 import wsgiref.handlers
+import string
 
+from django.utils import simplejson
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext import db
@@ -15,74 +17,124 @@ class Welcome(webapp.RequestHandler):
       
 class BrowseRecipe(webapp.RequestHandler):
     def get(self):
-        template_value = {"pageTitle": "Browse Family Recipes"}
+        template_value = {"pageTitle": "Browse Family Recipes from XML"}
         path = os.path.join(os.path.dirname(__file__), "template/browse.html")
         self.response.out.write(template.render(path, template_value))
         
-    def post(self):
-        userIp = self.request.get("ip")
-        feeling = self.request.get("feeling")
-        name = self.request.get("name")
-        email = self.request.get("email")
-        note = self.request.get("note")
-        
-        newFeel = MyFeel()
-        newFeel.userIp = userIp
-        newFeel.feeling = feeling
-        if len(name) > 0:
-            newFeel.name = name
-        if len(email) > 0:
-            newFeel.email = email
-        if len(note) > 0:
-            newFeel.note = note
-        newFeel.country = "China"
-        newFeel.put()
-        
-        self.response.out.write("status: %s, detail: %s" % ("success", userIp))
+class BrowseRecipe2(webapp.RequestHandler):
+    def get(self):
+        template_value = {"pageTitle": "Browse Family Recipes from Database"}
+        path = os.path.join(os.path.dirname(__file__), "template/browse2.html")
+        self.response.out.write(template.render(path, template_value))
 
+class PostRecipe(webapp.RequestHandler):
+    def get(self):
+        recipes = db.GqlQuery("SELECT * FROM RecipeModel ORDER BY name LIMIT 10")
+        
+        results = {}
+        i = 0
+        for recipe in recipes:
+            result = {}
+            result["name"] = recipe.name
+            result["category"] = recipe.category
+            result["type"] = recipe.type
+            result["hot"] = recipe.hot
+            result["count"] = recipe.count
+            result["sprint"] = recipe.sprint
+            result["summer"] = recipe.summer
+            result["fall"] = recipe.fall
+            result["winter"] = recipe.winter
+            result["combine"] = " ".join(recipe.combine)
+            result["rate"] = recipe.rate
+            result["source"] = " ".join(recipe.source)
+            result["other"] = recipe.other
+            results[i] = result
+            i = i + 1
+            
+        k = simplejson.dumps(results);
+        self.response.out.write(k)
+        
+    def post(self):
+        name = self.request.get("ip_name")
+        category = self.request.get("ip_category")
+        type = self.request.get("ip_type")
+        hot = self.request.get("ip_hot")
+        count = self.request.get("ip_count")
+        sprint = self.request.get("ip_sprint")
+        summer = self.request.get("ip_summer")
+        fall = self.request.get("ip_fall")
+        winter = self.request.get("ip_winter")
+        combine = self.request.get("ip_combine")
+        rate = self.request.get("ip_r")
+        source = self.request.get("ip_source")
+        other = self.request.get("ip_other")
+            
+        nr = RecipeModel()
+        nr.name = name
+        nr.category = category
+        nr.type = type
+        if (hot):
+            nr.hot = True
+        else:
+            nr.hot = False
+        nr.count = string.atoi(count)
+        if (sprint):
+            nr.sprint = True
+        else:
+            nr.sprint = False
+        if (summer):
+            nr.summer = True
+        else:
+            nr.summer = False
+        if (fall):
+            nr.fall = True
+        else:
+            nr.fall = False
+        if (winter):
+            nr.winter = True
+        else:
+            nr.winter = False
+        nr.rate = string.atoi(rate)
+        nr.combine = combine.split(" ")
+        nr.source = source.split(" ")
+        nr.other = other
+        
+        nr.put()
+        self.redirect("/browse2")
+        
 class AddRecipe(webapp.RequestHandler):
     def get(self):
         template_value = {"pageTitle": "Add New Recipes"}
         path = os.path.join(os.path.dirname(__file__), "template/add.html")
         self.response.out.write(template.render(path, template_value))	
 
-#		userIp = db.StringProperty()
-#	    feeling = db.StringProperty()
-#	    name = db.StringProperty()
-#	    email = db.EmailProperty()
-#	    country = db.StringProperty()
-#	    note = db.StringProperty(multiline = True)
-#	    date = db.DateProperty(auto_now_add = True)
-
 class SearchRecipe(webapp.RequestHandler):
     def get(self):
 		self.response.out.write("This is a test page for SearchRecipe service used by client.")
-	
-#        feelings = db.GqlQuery("SELECT * FROM MyFeel ORDER BY date LIMIT 10")
-        
-#        r = "value:"
-#        for feel in feelings:
-#            r += feel.userIp
-#            print r
-#            #self.response.out.write("%s", r)
-#            return
-#            r += "userIp:" + feel.userIp + ";"
-#            r += "feeling:" + feel.feeling + ";"
-#            r += "name:" + feel.name + ";"
-#            r += "email:" + feel.email + ";"
-#            r += "country:" + feel.country + ";"
-#            r += "note:" + feel.note + ";"
-#            r += "date:" + feel.date + ";"
-            
-#        self.response.out.write("%s", r)
-	
+
+class RecipeModel(db.Model):
+    name = db.StringProperty()
+    category = db.StringProperty()
+    type = db.StringProperty()
+    hot = db.BooleanProperty()
+    count = db.IntegerProperty()
+    sprint = db.BooleanProperty()
+    summer = db.BooleanProperty()
+    fall = db.BooleanProperty()
+    winter = db.BooleanProperty()
+    combine = db.StringListProperty()
+    rate = db.IntegerProperty()
+    source = db.StringListProperty()
+    other = db.StringProperty(multiline = True)
       
 def main():
   application = webapp.WSGIApplication([
 										('/', Welcome),
 										('/browse', BrowseRecipe),
+                                        ('/browse2', BrowseRecipe2),
 										('/add', AddRecipe),
 										('/search', SearchRecipe),
+                                        ('/postnew', PostRecipe),
 										],
 										debug=True)
   wsgiref.handlers.CGIHandler().run(application)
---
+++ Python/Project/Python Project/GoogleAppEngine/family-recipe/FamilyRecipe/template/js/browse2.js
@@ -0,0 +1,316 @@
+/**
+ * @author Larry
+ */
+
+$(document).ready (function(){
+	
+	/* put page init function here */
+	//alert("page load...");
+	
+	$("#btnTest").click (function()
+	{
+		$("#d_name").text("abc");
+	})
+	
+	// hook up listener for filter
+	$("#ip_show_all").click (function()
+	{
+		if ($(this)[0].checked == true)
+		{
+			$("#cb_type").attr("disabled", "true");
+			$("#cb_season").attr("disabled", "true");
+			$("#cb_rate").attr("disabled", "true");
+		}
+		else
+		{
+			$("#cb_type").removeAttr("disabled");
+			$("#cb_season").removeAttr("disabled");
+			$("#cb_rate").removeAttr("disabled");
+		}
+		updateDishs();	
+	})
+	
+	$("#cb_type").change (function()
+	{
+		updateDishs();
+	})
+	
+	$("#cb_season").change (function()
+	{
+		updateDishs();
+	})
+	
+	$("#cb_rate").change (function()
+	{
+		updateDishs();
+	})
+	
+	// var define
+	var dishXML;
+	var result_table_head_str = "<tr><th width='200' scope='col'>菜名</th><th width='80' scope='col'>类别</th><th width='80' scope='col'>种类</th><th width='160' scope='col'>级别</th><th width='80' scope='col'>使用次数</th></tr>";
+	
+	// disable all select options
+	$("#cb_type").attr("disabled", "true");
+	$("#cb_season").attr("disabled", "true");
+	$("#cb_rate").attr("disabled", "true");
+	
+	// load xml file
+	loadXMLFile();
+
+	// work function
+	function loadXMLFile()
+	{
+		$.ajax({
+			type: "GET",
+			url: "/postnew",
+			data: "",
+			success: dataLoaded
+		})
+	}
+	
+	function dataLoaded(data)
+	{
+		// add talbe head
+		$("#result_table").append(result_table_head_str);
+		
+		var obj = $.json.decode(data)
+		for (var i in obj)
+		{
+			var r = obj[i];
+			
+			var dishName = r.name;
+			var category = r.category;
+			var dishType = r.type;
+			var rate = r.rate;
+			var usedTimes = r.count;
+			
+			var t = "<tr><td>" + dishName + "</td><td>"
+								+ category + "</td><td>"
+								+ dishType + "</td><td>"
+								+ rate + "</td><td>"
+								+ usedTimes + "</td></tr>";
+								
+			$("#result_table").append(t);
+		}
+		
+		// format table to make it a little nicer
+		$("#result_table tr:odd").css("background-color", "#CCCCCC");
+		
+		// add table row click listener
+		//$("#result_table tr").click(dishClick);
+	}
+	
+	function updateDishs()
+	{
+		return;
+		// find related dishs
+		var d;
+		if ($("#ip_show_all")[0].checked == true)
+		{
+			d = $(dishXML).find("Dish");
+		}
+		else
+		{
+			d = chooseDishs($("#cb_type")[0].value, $("#cb_season")[0].value, $("#cb_rate")[0].value);
+			//alert("Filter dish by " + $("#cb_type")[0].value + ", " + $("#cb_season")[0].value + ", " + $("#cb_rate")[0].value + ", result: " + d.length);
+		}
+		
+		// update dishs to table
+		$("#result_table tr").remove();
+		
+		// add talbe head
+		$("#result_table").append(result_table_head_str);
+			
+		// add dishs
+		for (var i = 0; i < d.length; ++i)
+		{
+			var dishName = $(d[i]).children("DishName");
+			var dnTxt = formatString(dishName.text());
+			var category = $(d[i]).children("Category");
+			var dishType = $(d[i]).children("DishType");
+			var rate = $(d[i]).children("Rate");
+			var usedTimes = $(d[i]).children("UsedTimes");
+			
+			var t = "<tr><td>" + dnTxt + "</td><td>"
+								+ category.text() + "</td><td>"
+								+ dishType.text() + "</td><td>"
+								+ rate.text() + "</td><td>"
+								+ usedTimes.text() + "</td></tr>";
+								
+			$("#result_table").append(t);
+		}
+		
+		// format table to make it a little nicer
+		$("#result_table tr:odd").css("background-color", "#CCCCCC");
+		
+		// add table row click listener
+		$("#result_table tr").click(dishClick);
+	}
+	
+	function chooseDishs(type, season, rate)
+	{
+		var d = [];
+		$(dishXML).find("Dish").each(function(i){
+			var category = $(this).children("Category");
+			var ratevalue = $(this).children("Rate");
+			
+			// get season inside the Dish tag
+			var sArray = [];
+			$(this).find("Season").each(function(i){
+				sArray.push($(this).text());
+			})
+			
+			var typeok = true;
+			if (type != "all")
+			{
+				typeok = (category.text() == type);
+			}
+			
+			var seasonok = true;
+			if (season != "all")
+			{
+				seasonok = false;
+				for (var i = 0; i < sArray.length; ++i)
+				{
+					if (sArray[i] == season)
+					{
+						seasonok = true;
+						break;
+					}
+				}
+			}
+			
+			var rateok = true;
+			if (rate != "all")
+			{
+				rateok = (ratevalue.text() == rate);
+			}
+			
+			if (typeok == true && seasonok == true && rateok == true)
+			{
+				d.push(this);
+			}
+		});
+		return d;
+	}
+	
+	function dishClick(e)
+	{
+		e.stopPropagation();
+		var t = e.currentTarget;
+		if (t.rowIndex == 0) return;
+		
+		// find selected dish from xml
+		var dish = $(dishXML).find("Dish").eq(t.rowIndex - 1);
+		var dishName = $(dish).children("DishName");
+		var dnTxt = formatString(dishName.text());
+		var rate = $(dish).children("Rate").text();
+		var category = $(dish).children("Category").text();
+		var dishType = $(dish).children("DishType").text();
+		var usedTimes = $(dish).children("UsedTimes").text();
+		var isHot = $(dish).children("IsHot").text();
+		var otherNote = $(dish).children("OtherNotes").text();
+		otherNote = formatString(otherNote);
+		var s = combineArray(dish, "Season", getSeason);
+		var c = combineArray(dish, "Content");
+		var n = combineArray(dish, "Name");
+
+		$("#d_name").text(dnTxt);
+		$("#d_category").text(category);
+		$("#d_type").text(dishType);
+		$("#d_hot").text((isHot == "true") ? "是" : "否");
+		$("#d_count").text(usedTimes);
+		$("#d_month").text(s);
+		$("#d_combine").text(n);
+		$("#d_rate").text(rate);
+		$("#d_source").text(c);
+		$("#d_other").text(otherNote);
+		
+		openDishDetail();
+	}
+	
+	function removeExistingContents()
+	{
+		$("#result_table").remove();
+	}
+	
+	function openDishDetail()
+	{
+		$("#dish_detail_panel").removeClass("hidePanel");
+		$("#dish_detail_panel").bind("dialogbeforeclose", beforeDishClose);
+		$("#dish_detail_panel").dialog({autoOpen: false, resizable: false, width: 620});
+		$("#dish_detail_panel").dialog("option", "modal", true);
+		$("#dish_detail_panel").dialog("open");
+	}
+	
+	function beforeDishClose(event, ui)
+	{
+		// TODO: update dish detail
+	}
+	
+	// ======================================
+	// [HELP FUNCTION]
+	
+	// this function is used to remove "<![[CDATA]]>"
+	function formatString(str)
+	{
+		if (str[0] == "<" && str[5] == "A")
+		{
+			var s = "";
+			for (var i = 9; i < str.length - 3; ++i)
+			{
+				s += str[i];
+			}
+			return s;
+		}
+		else
+		{
+			return str;
+		}
+	}
+	
+	function combineArray(val, tag, otherFn)
+	{
+		//var sArray = [];
+		var s = "";
+		val.find(tag).each(function(i){
+			//sArray.push($(this).text());
+			var t = formatString($(this).text());
+			if (otherFn != undefined) t = otherFn(t);
+			s += t + " ";
+		})
+		s = $.trim(s);
+		return s;
+	}
+	
+	function getSeason(m)
+	{
+		if (m == "1")
+		{
+			return "春";
+		}
+		if (m == "2")
+		{
+			return "夏";
+		}
+		if (m == "3")
+		{
+			return "秋";
+		}
+		if (m == "4")
+		{
+			return "冬";
+		}
+	}
+	
+	function printObj(obj)
+	{
+		$("#objDetail tr").remove();
+		for (var v in obj)
+		{
+			var t = "<tr><td>" + v + "</td><td>" + obj[v] + "</td><td>";			
+			$("#objDetail").append(t);
+		}
+	}
+	
+})
