2a4efa9fb6c7f8d6e1ceaecf1518a56e200909c9
larryzzl <larryzzl@dd12737e-0f7a-11de-aa17-9100d24c02a7>
2010-04-25 08:48:40 +0000

    hook up server-client, database based workflow

--- trunk/Python/Project/Python Project/GoogleAppEngine/family-recipe/FamilyRecipe/template/js/add.js
+++ trunk/Python/Project/Python Project/GoogleAppEngine/family-recipe/FamilyRecipe/template/js/add.js
@@ -4,8 +4,54 @@
 
  $(document).ready (function(){
  	
-	//alert("asdf");
+	$("#errorHead").hide();
 	
+	$("#form_basic_info").validate({
+		//debug: true,
+		rules: {
+			ip_name: {
+				required: true
+			},
+			ip_count: {
+				required: true,
+				digits: true
+			}
+		},
+		messages: {
+			ip_name: "请输入菜名",
+			ip_count: "请输入使用次数"
+		},
+		//errorClass: "formHint",
+		//errorElement: "span",
+		//wrapper: "p",
+		invalidHandler: function(form, validator){
+			var errors = validator.numberOfInvalids();
+			if (errors)
+			{
+				var msg = "You have " + errors + "error input to fix!";
+				$("#errorHead").slideDown(500);
+			}
+			else
+			{
+				$("#errorHead").hide();
+			}
+		},
+		
+		errorPlacement: function(error, element){
+			if (element.parent("div").find("label").length == 1)
+			{
+				error.addClass("formHint");
+				error.addClass("errorElement");
+				error.removeClass("error");
+				error.appendTo(element.parent("div"));
+			}
+		},
+		/*
+		submitHandler: function(){
+			alert("Form is validated!");
+		},
+		*/
+	});
 	
 	
  })
